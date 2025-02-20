52579343a43e57d9749963b8810e3d30f47b690d
Anshul Hudda <anshulhudda.ssap@gmail.com>
2025-02-20 17:22:49 +0530

    Add base code

---
+++ src/llama-index/app.py
@@ -0,0 +1,91 @@
+
+from typing import Any
+import pandas as pd
+from llama_index.core import Document
+import os
+import re
+from dotenv import load_dotenv
+from llama_index.llms.openai import OpenAI
+from llama_index.core.node_parser import SentenceSplitter
+
+from extractor import GraphRAGExtractor
+from extractor_prompt import KG_TRIPLET_EXTRACT_TMPL
+from store import GraphRAGStore
+from query_engine import GraphRAGQueryEngine
+
+
+load_dotenv()
+
+os.env = os.getenv("OPENAI_API_KEY")
+
+news = pd.read_csv(
+    "https://raw.githubusercontent.com/tomasonjo/blog-datasets/main/news_articles.csv"
+)[:50]
+
+news.head()
+
+
+documents = [
+    Document(text=f"{row['title']}: {row['text']}")
+    for i, row in news.iterrows()
+]
+
+llm = OpenAI(model="gpt-4")
+
+
+splitter = SentenceSplitter(
+    chunk_size=1024,
+    chunk_overlap=20,
+)
+nodes = splitter.get_nodes_from_documents(documents)
+
+entity_pattern = r'\("entity"\$\$\$\$"(.+?)"\$\$\$\$"(.+?)"\$\$\$\$"(.+?)"\)'
+relationship_pattern = r'\("relationship"\$\$\$\$"(.+?)"\$\$\$\$"(.+?)"\$\$\$\$"(.+?)"\$\$\$\$"(.+?)"\)'
+
+
+def parse_fn(response_str: str) -> Any:
+    entities = re.findall(entity_pattern, response_str)
+    relationships = re.findall(relationship_pattern, response_str)
+    return entities, relationships
+
+
+kg_extractor = GraphRAGExtractor(
+    llm=llm,
+    extract_prompt=KG_TRIPLET_EXTRACT_TMPL,
+    max_paths_per_chunk=2,
+    parse_fn=parse_fn,
+)
+
+from llama_index.graph_stores.neo4j import Neo4jPropertyGraphStore
+
+# Note: used to be `Neo4jPGStore`
+graph_store = GraphRAGStore(
+    username=os.getenv("NEO4J_USERNAME"), password=os.getenv("NEO4J_PASSWORD"), url=os.getenv("NEO4J_URI")
+)
+
+from llama_index.core import PropertyGraphIndex
+
+index = PropertyGraphIndex(
+    nodes=nodes,
+    kg_extractors=[kg_extractor],
+    property_graph_store=graph_store,
+    show_progress=True,
+)
+
+index.property_graph_store.build_communities()
+
+
+
+query_engine = GraphRAGQueryEngine(
+    graph_store=index.property_graph_store,
+    llm=llm,
+    index=index,
+    similarity_top_k=10,
+)
+
+
+response = query_engine.query(
+    "What are the main news discussed in the document?"
+)
+
+print(response)
